86eb74eaa44135f89352bd66bad6fe24c217f93d
Holden Karau <holden@pigscanfly.ca>
2014-02-23 17:33:11 -0800

    Code review follow up.

--- core/src/main/scala/org/apache/spark/util/random/RandomSampler.scala
+++ core/src/main/scala/org/apache/spark/util/random/RandomSampler.scala
@@ -62,6 +62,8 @@ class BernoulliSampler[T](lb: Double, ub: Double, complement: Boolean = false)
     }
   }
 
+  def cloneComplement() = new BernoulliSampler[T](lb, ub, !complement)
+
   override def clone = new BernoulliSampler[T](lb, ub, complement)
 }
 
